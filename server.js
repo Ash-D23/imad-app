f8e2a9bf13824eb5bbcda55c7a32f2609bc72ad5
Ash-D23 <30368163+Ash-D23@users.noreply.github.com>
2017-08-15 18:16:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ app.get('/article-three',function(req, res){
   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
+app.get('/article',function(req, res){
+  res.sendFile(path.join(__dirname, 'ui', 'article.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
